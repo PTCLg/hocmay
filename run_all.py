6e96c9e152432a2e925f721c181174581c8a2d1a
PTCLg <2251172460.e.tlu.edu.vn>
2024-09-13 20:29:31 +0700

    update run all, librarys

--- run_all.py
+++ run_all.py
@@ -2,14 +2,18 @@
 
 import subprocess
 
+# Cài đặt các thư viện từ requirements.txt
+print("Installing dependencies from requirements.txt...")
+subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
+
 # Chạy create_dataset.py
 print("Running create_dataset.py...")
-subprocess.run(["python", "create_dataset.py"])
+subprocess.run(["python", "create_dataset.py"], check=True)
 
 # Chạy train_model.py
 print("Running train_model.py...")
-subprocess.run(["python", "train_model.py"])
+subprocess.run(["python", "train_model.py"], check=True)
 
 # Chạy app.py
 print("Running app.py...")
-subprocess.run(["python", "app.py"])
+subprocess.run(["python", "app.py"], check=True)
