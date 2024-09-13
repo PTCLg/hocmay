912d7a6fb7909744ce68ad4fd5fa9e3978c109c2
PTCLg <2251172460.e.tlu.edu.vn>
2024-09-13 20:43:51 +0700

    add run all

---
+++ run_all.py
@@ -0,0 +1,19 @@
+# run_all.py
+
+import subprocess
+
+# # Cài đặt các thư viện từ requirements.txt
+# print("Installing dependencies from requirements.txt...")
+# subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
+
+# Chạy create_dataset.py
+print("Running create_dataset.py...")
+subprocess.run(["python", "create_dataset.py"], check=True)
+
+# Chạy train_model.py
+print("Running train_model.py...")
+subprocess.run(["python", "train_model.py"], check=True)
+
+# Chạy app.py
+print("Running app.py...")
+subprocess.run(["python", "app.py"], check=True)
